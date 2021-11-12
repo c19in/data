7f510d4b250aae947ae439f2f1cfd413da892267
Deepayan Sarkar <deepayan.sarkar@gmail.com>
2021-11-12 11:05:05 +0530

    updates

--- docs/prettyjson/incovid19/update.sh
+++ docs/prettyjson/incovid19/update.sh
@@ -15,14 +15,14 @@ for JSONFILE in timeseries.min.json data.min.json; do
 done
 
 for STATECODE in AP AR AS BR CT GA GJ HR HP JH KA KL MP MH MN ML MZ NL \
-		 OR PB RJ SK TN TG TR UT UP WB AN CH DN DL JK LA LD PY; do
+		 OR PB RJ SK TN TG TR UT UP WB AN CH DN DL JK LA LD PY TT; do
     JSONFILE="timeseries-${STATECODE}.min.json"
     echo "Downloading ${JSONFILE}"
     wget --no-check-certificate -a "WGET_${CTIME}.log" -O "tmp/${JSONFILE}" "${JSONDATASRC}/${JSONFILE}"
 done
 
 for STATECODE in AP AR AS BR CT GA GJ HR HP JH KA KL MP MH MN ML MZ NL \
-		 OR PB RJ SK TN TG TR UT UP WB AN CH DN DL JK LA LD PY; do
+		 OR PB RJ SK TN TG TR UT UP WB AN CH DN DL JK LA LD PY TT; do
     INFILE="tmp/timeseries-${STATECODE}.min.json"
     OUTFILE="timeseries-${STATECODE}.min.json"
     echo "Converting ${OUTFILE}"
